b0d286744d33dee93dc6778fcfc74f9f2059e242
Timothé Cordier <timothe.cordier@gmail.com>
2024-03-01 14:41:58 +0100

    final files

--- app.js
+++ app.js
@@ -1,59 +1,75 @@
 // API ENDPOINT : `https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srlimit=20&srsearch=${searchInput}`
 
-const submit = document.querySelector("form");
-const input = document.querySelector("input");
-const resultContainer = document.querySelector(".result");
+const form = document.querySelector("form");
 const error = document.querySelector(".error-msg");
+const input = document.querySelector("input");
+const resultContainer = document.querySelector(".result-container");
+const loader = document.querySelector(".container-dot");
 
-submit.addEventListener("submit", handleSubmit);
+form.addEventListener("submit", handleSubmit);
 
 function handleSubmit(e) {
   e.preventDefault();
   if (input.value === "") {
-    input.value = "Veuillez remplir le champs 😊";
+    input.value = "Veuillez remplir le champ 😊";
+    input.classList.toggle("active");
+    input.addEventListener("focus", () => {
+      input.value = "";
+    });
+    return;
   } else {
-    getArticle(input.value);
+    resultContainer.textContent = "";
+    error.textContent = "";
+    input.addEventListener("focus", () => {
+      input.value = "";
+      error.textContent = "";
+    });
+    input.addEventListener("keypress", () => {
+      input.classList.remove("active");
+    });
+    loader.style.display = "flex";
+    setTimeout(() => connectArticle(input.value), 1500);
   }
 }
 
-async function getArticle(input) {
+async function connectArticle(input) {
+  let data;
   try {
     const response = await fetch(
-      `https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srlimit=20&srsearch=${input}`
-    );
-    console.log(response);
-    if (!response.ok) {
-      throw Error(`${response.status}`);
-    }
+      `https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srlimit=20&srsearch=${input}`)
+    if(!response.ok) {
+      throw new Error(`${response.status} 😿`);
+    };
 
-    const data = await response.json();
-    console.log(data);
-    createArticle(data.query.search);
-  } catch (error) {
-    error.textContent = `Error : ${error.message}`;
-    console.dir(error);
+    data = await response.json();
+    getArticle(data.query.search);
+    loader.style.display = "none";
+
+  } catch(Error) {
+    error.textContent = `${Error}`;
+    loader.style.display = "none";
+
   }
 }
 
-function createArticle(data) {
+function getArticle(data) {
   if (!data.length) {
-    error.textContent = "Woops, aucune réponse trouvée... ☹️";
+    error.textContent = "Wops, recherche introuvable ☹️";
+    resultContainer.textContent = "";
+    return;
   }
-  data.forEach(el => {
+  data.forEach((el) => {
     const url = `https://en.wikipedia.org/?curid=${el.pageid}`;
-    const card = document.createElement("div");
-    console.log(card);
-    card.className = "result-card";
-    card.innerHTML = `
-        <h3 class="result-title">
-            <a href="${url} target="_blank">${el.title}</a>
-        </h3>
-        <a class="result-link" href="${url} target ="_blank">${url}</a><br>
-        <span class="result-text">${el.snippet}</span>
-        `;
-    resultContainer.appendChild(card);
+    const result = document.createElement("div");
+    result.className = "result-card";
+    result.innerHTML = `
+    <h3 class="result-title">
+    <a href="${url}">${el.title}</a>
+    </h3>
+    <a class="result-link" href="${url}">${url}</a><br>
+    <span class="result-text">${el.snippet}</span>
+    <hr>
+    `;
+    resultContainer.appendChild(result);
   });
 }
-
-/* <a href="${url}>${url}</a>
-<span class="result-text">${el.snippet}</span> */
